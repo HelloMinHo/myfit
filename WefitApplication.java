68e943dc1b252b9c2aef61759267bf25b5a65f1a
HelloMinHo <103483954+HelloMinHo@users.noreply.github.com>
2022-12-08 17:43:37 +0900

    Add files via upload

---
+++ WefitApplication.java
@@ -0,0 +1,16 @@
+package com.kmh.wefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WefitApplication.class, args);
+
+
+
+	}
+
+}
